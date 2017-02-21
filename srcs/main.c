582eb3002d8b0445c491be84b4d3cb09f29583d3
Jules LASNE <jlasne@student.42.fr>
2017-02-21 13:18:42 +0100

    Cleanup + TODO list

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: blucas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 11:32:46 by blucas            #+#    #+#             */
-/*   Updated: 2017/02/21 12:45:12 by jlasne           ###   ########.fr       */
+/*   Updated: 2017/02/21 13:17:58 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,29 @@ int		main(int argc, char **argv)
 	return (0);
 }
 
-//Return when -R after folder name
 //leaks
 //tri t r
 //segfault when impossible to access dir (/dev/fd/3 for example)
+//faire un perror quand appel system fail
+//./ft_ls ""
+//perror + errno
+//./ft_ls "- a" == exit_failure
+//readlink pour liens (ln -s toto; ft_ls -l n'affche rien, ./ft_ls toto = segfault, ./ft_ls -l toto - segfault
+//./ft_ls srcs Makefile = segfault
+//
+//Ne pas afficher le poids dans major minmor /dev
+//
+//Dans l'odre : Erreur, fichier, repertoire (ordre par option passe)
+//exemple : ls tutu srcs Makefile
+//Optionnel : Allignement majorminor
+//
+//./ft_ls /dev -a
+//./ft_ls -a -- srcs
+//gestion de -- (fin de flag = tout ce qui est apres est gere comme dossier)
+//
+//
+//bonus easy
+//ls -s1
+//ls -g
+//ls -G
+//ls -o
